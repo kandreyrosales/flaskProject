ed546a771d2de63257e0e3ed3a54abba62342756
Kevin Rosales <kevin.rosales@optimizely.com>
2021-08-26 15:49:23 -0500

    changes

--- test/test_fibonacci_app.py
+++ test/test_fibonacci_app.py
@@ -4,6 +4,10 @@ from app import complete_fibonacci
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+        """
+        Test for Virtualmind project
+        :return: List of numbers
+        """
         self.assertEqual(complete_fibonacci(5), [0, 1, 1, 2, 3])
 
 
